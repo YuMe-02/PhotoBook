9bda2582fd49938bde040a37f2f1833be76f828e
YuMe-02 <darrenyu90@gmail.com>
2025-05-04 11:13:12 -0700

    boiler plate and NavBar

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="NavBar">
+
+      <div className = "Name">
+        Darren Yu
+      </div>
+
+      <ul>
+
+      </ul>
+
+
+      <footer>
+        Joe MaMa
+      </footer>
+
+
+    </div>
+  );
+}
+
+export default App;
